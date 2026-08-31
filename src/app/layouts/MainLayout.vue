9ec3a6aae3ce55357553013f55d529a534a9a87c
Tinoicb <jason994842@gmail.com>
2026-08-31 21:38:41 +0800

    OK右邊清光光

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -4,15 +4,12 @@ import Navbar from '@/editor/navbar/Navbar.vue';
 import ProjectSidebar from '@/editor/sidebar/ProjectSidebar.vue';
 import ToolbarPanel from '@/editor/toolbar/ToolbarPanel.vue';
 import FactoryCanvas from '@/editor/canvas/FactoryCanvas.vue';
-import InspectorSidebar from '@/editor/inspector/InspectorSidebar.vue';
 import StatsPanel from '@/components/StatsPanel/Index.vue';
 import { useFlowEngine } from '@/composables/useFlowEngine';
 import { useValidation } from '@/composables/useValidation';
 
 /** 左側專案選單目前是否展開，預設收合 */
 const sidebarOpen = ref(false);
-/** 右側 Inspector 面板目前是否展開，預設展開 */
-const inspectorOpen = ref(true);
 
 /**
  * 啟動 CR-03 驗證監聽（藍圖變動時 sync 重跑所有 detector）。  \
@@ -44,8 +41,6 @@ useFlowEngine();
                     <StatsPanel />
                 </aside>
             </div>
-
-            <InspectorSidebar v-model:open="inspectorOpen" />
         </div>
     </div>
 </template>
