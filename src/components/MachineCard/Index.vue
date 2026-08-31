8ed7614b945803861ec2ebf18d8532bf51d5bc9c
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-08-31 14:37:32 +0800

    add target.svg

---
+++ src/components/MachineCard/Index.vue
@@ -0,0 +1,16 @@
+/* button */
+
+position: absolute;
+width: 24px;
+height: 39px;
+left: 437px;
+top: 369px;
+
+/* button */
+
+position: absolute;
+width: 24px;
+height: 39px;
+right: -10px;
+top: 369px;
+
