0eca87cb2ddcb1be5f20980e044e4f4921246bf2
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-09-01 23:46:21 +0800

    refactor(MachineCard): extract Item component and align operators

---
+++ src/components/MachineCard/components/Item.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import type { FormulaItem } from '../types';
+import defaultItemImage from './item.png';
+
+defineProps<{
+    item: FormulaItem;
+}>();
+</script>
+
+<template>
+    <div class="item">
+        <div class="plate">
+            <img :src="item.image || defaultItemImage" class="item-image" alt="item" />
+
+            <div v-if="item.amount !== undefined" class="num">
+                <span class="num-text">{{ item.amount }}</span>
+            </div>
+        </div>
+
+        <span v-if="item.name" class="item-name">{{ item.name }}</span>
+    </div>
+</template>
+
+<style scoped>
+/* item */
+.item {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3px;
+    width: 71.63px;
+    flex-shrink: 0;
+    box-sizing: border-box;
+}
+
+/* item-name */
+.item-name {
+    width: 71.63px;
+    min-height: 18.37px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    color: #cfcfcf;
+    white-space: nowrap;
+}
+
+/* plate */
+.plate {
+    box-sizing: border-box;
+    position: relative;
+    width: 55.1px;
+    height: 55.1px;
+    background: #2b2b2b;
+    border: 3px solid #eefd1c;
+    border-radius: 50%;
+    box-shadow:
+        0px 4px 4px rgba(0, 0, 0, 0.25),
+        inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+/* image */
+.item-image {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 55.1px;
+    height: 55.1px;
+    display: block;
+    z-index: 1;
+}
+
+/* num (數量標籤) */
+.num {
+    position: absolute;
+    right: -5px;
+    bottom: -2px;
+    width: 19px;
+    height: 20px;
+    background: #eefd1c;
+    border-radius: 50%;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 2;
+}
+
+.num-text {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 19px;
+    text-align: center;
+    color: #2b2b2b;
+}
+</style>
--- src/components/MachineCard/components/SingleFormula.vue
+++ src/components/MachineCard/components/SingleFormula.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
+import Item from './Item.vue';
 import type { Formula } from '../types';
-import defaultItemImage from './item.png';
 
 defineProps<{
     singleformula: Formula;
@@ -14,36 +14,14 @@ defineProps<{
         <div class="formula-row">
             <template v-for="(item, index) in singleformula.input" :key="`in-${index}`">
                 <span v-if="index > 0" class="operator plus">+</span>
-
-                <div class="item">
-                    <div class="plate">
-                        <img :src="item.image || defaultItemImage" class="item-image" alt="item" />
-
-                        <div v-if="item.amount !== undefined" class="num">
-                            <span class="num-text">{{ item.amount }}</span>
-                        </div>
-                    </div>
-
-                    <span v-if="item.name" class="item-name">{{ item.name }}</span>
-                </div>
+                <Item :item="item" />
             </template>
 
             <span class="operator arrow">→</span>
 
             <template v-for="(item, index) in singleformula.output" :key="`out-${index}`">
                 <span v-if="index > 0" class="operator plus">+</span>
-
-                <div class="item">
-                    <div class="plate">
-                        <img :src="item.image || defaultItemImage" class="item-image" alt="item" />
-
-                        <div v-if="item.amount !== undefined" class="num">
-                            <span class="num-text">{{ item.amount }}</span>
-                        </div>
-                    </div>
-
-                    <span v-if="item.name" class="item-name">{{ item.name }}</span>
-                </div>
+                <Item :item="item" />
             </template>
         </div>
     </div>
@@ -89,88 +67,10 @@ defineProps<{
     display: none;
 }
 
-/* item */
-.item {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 3px;
-    width: 71.63px;
-    flex-shrink: 0;
-    box-sizing: border-box;
-}
-
-/* item-name */
-.item-name {
-    width: 71.63px;
-    min-height: 18.37px;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 19px;
-    text-align: center;
-    color: #cfcfcf;
-    white-space: nowrap;
-}
-
-/* plate */
-.plate {
-    box-sizing: border-box;
-    position: relative;
-    width: 55.1px;
-    height: 55.1px;
-    background: #2b2b2b;
-    border: 3px solid #eefd1c;
-    border-radius: 50%;
-    box-shadow:
-        0px 4px 4px rgba(0, 0, 0, 0.25),
-        inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-/* image */
-.item-image {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 55.1px;
-    height: 55.1px;
-    display: block;
-    z-index: 1;
-}
-
-/* num (數量標籤) */
-.num {
-    position: absolute;
-    right: -5px;
-    bottom: -2px;
-    width: 19px;
-    height: 20px;
-    background: #eefd1c;
-    border-radius: 50%;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 2;
-}
-
-.num-text {
-    font-style: normal;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 19px;
-    text-align: center;
-    color: #2b2b2b;
-}
-
 .operator {
-    width: 10px;
-    height: 19px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     font-style: normal;
     font-weight: 700;
     font-size: 16px;
@@ -178,19 +78,16 @@ defineProps<{
     text-align: center;
     color: #cfcfcf;
     user-select: none;
-    flex: none;
-    flex-grow: 0;
     flex-shrink: 0;
+}
+
+.operator.plus {
+    width: 10px;
+    height: 19px;
 }
 
 .operator.arrow {
     width: 35px;
     height: 19px;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    text-align: center;
-    color: #cfcfcf;
 }
 </style>
