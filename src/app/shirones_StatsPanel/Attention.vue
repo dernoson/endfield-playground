835fbc58e11ed8d31f656de9f5314e6f79a7fee0
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-09-18 03:08:03 +0800

    feat(shirones_StatsPanel): 對齊 Attention Warning 圖層架構與樣式規範

--- src/app/shirones_StatsPanel/Attention.vue
+++ src/app/shirones_StatsPanel/Attention.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import iconUrl from './ErrorIcon.svg';
+import errorIconUrl from './ErrorIcon.svg';
+import warningIconUrl from './WarningIcon.svg';
 
 interface Props {
     errorMessage?: string;
@@ -20,25 +21,17 @@ withDefaults(defineProps<Props>(), {
         <!-- 2. Error = [plate + icon + text + text + text ... ] -->
         <div class="error">
             <div class="plate" />
-            <img :src="iconUrl" class="icon" alt="error icon" />
+            <img :src="errorIconUrl" class="icon" alt="error icon" />
             <div class="text">
                 <slot name="error">{{ errorMessage }}</slot>
             </div>
         </div>
 
-        <!-- 3. Warning (Plate, Icon, Text) -->
+        <!-- 3. Warning = [plate + icon + text + text + text ... ] -->
         <div class="warning">
             <div class="plate" />
-            <div class="icon">
-                <svg viewBox="0 0 24 24" fill="currentColor">
-                    <path
-                        fill-rule="evenodd"
-                        d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                        clip-rule="evenodd"
-                    />
-                </svg>
-            </div>
-            <div class="warning-text">
+            <img :src="warningIconUrl" class="icon" alt="warning icon" />
+            <div class="text">
                 <slot name="warning">{{ warningMessage }}</slot>
             </div>
         </div>
@@ -99,65 +92,68 @@ withDefaults(defineProps<Props>(), {
 
 /* Text inside Error */
 .error .text {
-    position: absolute;
-    left: 42px;
-    top: 7px;
-
     font-family: 'HarmonyOS Sans TC', sans-serif;
     font-style: normal;
     font-weight: 400;
     font-size: 18px;
     line-height: 21px;
+    
+    position: absolute;
+    left: 42px;
+    top: 7px;
 
     color: #ff6e6e;
     white-space: nowrap;
     z-index: 1;
 }
 
-/* 3. Warning */
+/* 3. Warning = [plate + icon + text + text + text ... ] */
 .warning {
+    box-sizing: border-box;
     position: relative;
+    width: 100%;
+    height: 35px;
     display: flex;
     align-items: center;
-    padding: 8px 12px;
-    gap: 10px;
-    border-radius: 4px;
+    padding-left: 36px;
+    padding-right: 8px;
     overflow: hidden;
 }
 
+/* Plate */
 .warning .plate {
+    box-sizing: border-box;
     position: absolute;
     inset: 0;
-    background: #463e2a;
+    height: 35px;
+    background: #f7d9451a;
     z-index: 0;
 }
 
+/* Icon */
 .warning .icon {
-    position: relative;
+    position: absolute;
+    top: 7px;
+    left: 7px;
+    width: 26px;
+    height: 23px;
     z-index: 1;
-    width: 20px;
-    height: 20px;
-    flex-shrink: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #fbc02d;
 }
 
-.warning .icon svg {
-    width: 18px;
-    height: 18px;
-}
-
-.warning .warning-text {
-    position: relative;
-    z-index: 1;
+/* Text inside Warning */
+.warning .text {
     font-family: 'HarmonyOS Sans TC', sans-serif;
-    font-size: 13px;
-    font-weight: 500;
-    color: #f2ce47;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+
+    position: absolute;
+    left: 42px;
+    top: 7px;
+
+    color: #f7d945;
     white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+    z-index: 1;
 }
 </style>
